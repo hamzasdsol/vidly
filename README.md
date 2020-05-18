5b3fede123755de93a4bf493cf51d21d7aabe6bb
Khawaja Hamza Abid <khawajahamza302@gmail.com>
2020-05-18 18:31:39 +0500

    Added Object ID Validation

--- README.md
+++ README.md
@@ -2,3 +2,12 @@
 self Learning Project of Node js
 Cloned succecfully
 Added models folder
+
+Object ID in mongo Db consists of 24 characters and 12 bytes. ( every 2 characters reps 1 byte)
+
+// 4 bytes = time stamp
+// 3 bytes = machine identifier
+// 2 bytes = process identifier
+// 3 bytes = counter
+
+Added Object ID Validation
--- models/rental.js
+++ models/rental.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Joi = require('joi');
+Joi.objectId = require('joi-objectid')(Joi);
 
 const rentalSchema = new mongoose.Schema({
     customer: {
@@ -33,8 +34,8 @@ const Rental = mongoose.model('Rental', rentalSchema);
 
 function validateRental(rental){
     const schema = {
-        customerId: Joi.string().required(),
-        movieId : Joi.string().required()
+        customerId: Joi.objectId().required(),
+        movieId : Joi.objectId().required()
     };
 
     return Joi.validate(rental,schema);
