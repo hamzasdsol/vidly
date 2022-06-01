cf1fdb96b9edee40d4e90843e5366f4ecc245baa
SDSol <hamza.sdsol@gmail.com>
2022-06-01 16:51:35 +0500

    Object id validation middleware changes

--- middleware/validateObjectId.js
+++ middleware/validateObjectId.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
 module.exports = function (req, res, next) {
-    if (!mongoose.Types.ObjectId.isValid(req.body.ObjectId)) {
-        res.send(404).send('Invalid ID');
+    if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+        res.status(404).send('Invalid ID');
     }
 
     next();
